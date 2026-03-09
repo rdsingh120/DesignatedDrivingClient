05e34fe15ff5418eb4cd9a3d1a3177546a672918
DrBidoof <alwyn.lynch23@gmail.com>
2026-03-09 17:08:14 -0400

    Create ratingsClient.js

---
+++ src/api/ratingsClient.js
@@ -0,0 +1,10 @@
+import http from "./http";
+
+export const apiCreateRating = (data) =>
+  http.post("/ratings", data);
+
+export const apiGetUserRatings = (userId) =>
+  http.get(`/ratings/user/${userId}`);
+
+export const apiGetTripRatings = (tripId) =>
+  http.get(`/ratings/trip/${tripId}`);
